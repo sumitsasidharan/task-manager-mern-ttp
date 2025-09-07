39154c7d92a77195680d739d194fb65300e4c590
sumitsasidharan <s.sumit196@gmail.com>
2025-09-07 09:12:18 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="text-3xl">app</div>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Admin/ManageTasks.tsx
@@ -0,0 +1,7 @@
+const ManageTasks = () => {
+  return <div>ManageTasks</div>;
+};
+
+export default ManageTasks;
+
+// 17.49
